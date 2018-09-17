2a8bfc7858a78bb22936730542318a24d4d0e512
JMennenga <43140767+JMennenga@users.noreply.github.com>
2018-09-17 15:59:51 +0200

    return hinzugefügt

--- LR.py
+++ LR.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 
-np.matrix([[2,3,5],[6,12,22],[4,12,20]];
+Test = np.matrix([[2,3,5],[6,12,22],[4,12,20]];
 
 
 def LR(R):
     if np.size(R)[0]==np.size(R)[1]:
         L=np.eye(np.size(R))
         for i in range(np.size(R)[0]):
-            
-            for j in range(np.size(R)[1]):
+            for j in range(i+1,np.size(R)[1]):
+                a=R[j.i]/R[i,i]
+                L[j,i]=-a
+                for k in range(i+1,np.size(R)[1]):
+                    R[j,k]=R[j,k]-a*R[i,k]
+    return(L,R)
