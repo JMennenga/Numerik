52b66b08200bf0880ba32809035f5daba582bc5b
JMennenga <43140767+JMennenga@users.noreply.github.com>
2018-09-17 15:59:07 +0200

    erster entwurf

---
+++ LR.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+np.matrix([[2,3,5],[6,12,22],[4,12,20]];
+
+
+def LR(R):
+    if np.size(R)[0]==np.size(R)[1]:
+        L=np.eye(np.size(R))
+        for i in range(np.size(R)[0]):
+            
+            for j in range(np.size(R)[1]):
