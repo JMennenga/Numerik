907392519aba292f3f8524a3dc88fd2bff7fea6f
JMennenga <43140767+JMennenga@users.noreply.github.com>
2018-09-20 23:10:58 +0200

    nablamabla

---
+++ nabla.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy.linalg as lin
+
+#Diskretisierung des Nabla-Operators bezueglich des Einheitsquadrats
+def nablaEQ(func,h):
+
+    x=np.arange(h,1,h)
+    X,Y=np.meshgrid(x,x)
+    F=np.vectorize(func)(X,Y).reshape(:,1)
+    #F=np.array([func(x,y) for (x,y) in (X,Y)])
+    #zu Testzwecken evtl hier returnen
+    #return F
+    schema=np.zeros(x.size[0],x.size[0])
+    schema[0,0:2]=[-3,4,-1]
+    schema[x.size[0]-1,x.size[0]-3:x.size[0]-1]=[1,-4,3]
+    for i in range(1,x.size[0]):
+        schema[i,i-1:i+1]=[-1,0,1]
+    dx=(1/2h)*np
+
+def testfunc(arg1,arg2):
+    return arg1 + arg2
+
+print(nablaEQ(testfunc,0.1))
