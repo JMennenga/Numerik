68e5569785325b696a36bbe94ba98b15b34a0271
M-Munier <marc@geomaps.de>
2018-10-07 17:39:53 +0200

    rk4 func

--- test.py
+++ test.py
@@ -8,6 +8,52 @@ import scipy.sparse.linalg as splinalg
 import matplotlib.colors as colors
 from stencil import Ableitung
 
+
+def rhs(ww, t):
+    ww[b_rand] = 0
+    psi_innen = splinalg.spsolve(Lap1.matrix, (ww - Lap0.matrix * psi_rand))
+
+    u = D1y.matrix * (psi_rand + psi_innen)
+    plt.imshow(D1y.matrix.todense())
+    plt.show()
+    u[np.array(dir_rand & 0x3, dtype=bool)
+      ] = u_rand[np.array(dir_rand & 0x3, dtype=bool)]
+    v = -D1x.matrix * (psi_rand + psi_innen)
+    v[np.array(dir_rand & 0xC, dtype=bool)
+      ] = v_rand[np.array(dir_rand & 0xC, dtype=bool)]
+
+    ww_rand = Lap_rand * (psi_rand + psi_innen) + \
+        Neumann_Korrekturx * u + Neumann_Korrekturx * v
+
+    ax = u > 0
+    ay = v > 0
+
+    rhs = -((np.multiply(ax, D1w.dot(u * (ww + ww_rand))))
+            + (np.multiply(np.logical_not(ax), D1o.dot(u * (ww + ww_rand))))
+            + (np.multiply(ay, D1s.dot(v * (ww + ww_rand))))
+            + (np.multiply(np.logical_not(ay), D1n.dot(v * (ww + ww_rand))))
+
+            - kin_vis * (Lap0.matrix * (ww + ww_rand))
+            )
+    return rhs
+
+
+def rk4(f, ww, max_iter):
+
+    t = 0
+    dt = 0
+
+    for i in range(max_iter + 1):
+        k1 = f(ww, t+dt/2)
+        k2 = f(ww + k1*dt/2, t+dt/2)
+        k3 = f(ww + k2*dt/2, t+dt/2)
+        k4 = f(ww + k3*dt, t+dt)
+
+        ww += dt*(k1 + 2*k2 + 2*k3 + k4)/6
+        t += dt
+
+        yield ww
+
 kin_vis = 0.01
 h = 1
 loop_count = 0
@@ -61,23 +107,17 @@ xx = np.linspace(0, 1, gridshape[0])
 yy = np.linspace(0, 1, gridshape[1])
 XX, YY = np.meshgrid(xx, yy)
 
-WW = np.sin(0 * np.pi * (XX)) * np.sin(4 * np.pi * YY)
+WW = np.sin(1 * np.pi * (XX)) * np.sin(4 * np.pi * YY)
 ww = WW.reshape(gridlength)
-print('1')
+
 D1x = Ableitung(gridshape, 0, 1, 0, rand=b_rand)
-print('2')
 D1y = Ableitung(gridshape, 1, 1, 0, rand=b_rand)
-print('3')
 D1o = Ableitung(gridshape, 0, 1,  0.5, rand=b_rand)
-print('4')
 D1w = Ableitung(gridshape, 0, 1, -0.5, rand=b_rand)
-print('5')
 D1s = Ableitung(gridshape, 0, 1,  0.5, rand=b_rand)
-print('6')
 D1n = Ableitung(gridshape, 1, 1, -0.5, rand=b_rand)
-print('7')
+
 Lap0 = Ableitung(gridshape, 0, 2, 0, rand=b_rand)
-print('8')
 Lap0 = Lap0.add(Ableitung(gridshape, 1, 2, 0, rand=b_rand))
 
 Lap0.randmod(b_rand, 'r')
@@ -140,7 +180,7 @@ Lap1.final()
 plt.imshow(Lap1.matrix.todense())
 plt.show()
 # LLLLLLOOOOOOOOOOOOOPPPPPPPP
-plt.ion()
+#plt.ion()
 fig = plt.figure()
 
 
@@ -190,73 +230,6 @@ fig = plt.figure()
 #
 #     loop_count += 1
 # rk4
-loop_count = 0
-CFL = 0.8
-
-while (loop_count <= 1000):
-    dt = h * CFL/max(np.abs(np.append(u, v)))
-    for i in range(4):
-
-        if i == 1:
-            w = ww + rhs*dt/2
-        if i == 2:
-            w = ww + rhs*dt/2
-        if i == 3:
-            w = ww + rhs*dt
-
-        ww[b_rand] = 0
-        psi_innen = splinalg.spsolve(Lap1.matrix, (ww - Lap0.matrix * psi_rand))
-
-        u = D1y.matrix * (psi_rand + psi_innen)
-        u[np.array(dir_rand & 0x3, dtype=bool)
-          ] = u_rand[np.array(dir_rand & 0x3, dtype=bool)]
-        v = -D1x.matrix * (psi_rand + psi_innen)
-        v[np.array(dir_rand & 0xC, dtype=bool)
-          ] = v_rand[np.array(dir_rand & 0xC, dtype=bool)]
-
-        ww_rand = Lap_rand * (psi_rand + psi_innen) + \
-            Neumann_Korrekturx * u + Neumann_Korrekturx * v
-
-        ax = u > 0
-        ay = v > 0
-
-        rhs = -((np.multiply(ax, D1w.dot(u * (ww + ww_rand))))
-                + (np.multiply(np.logical_not(ax), D1o.dot(u * (ww + ww_rand))))
-                + (np.multiply(ay, D1s.dot(v * (ww + ww_rand))))
-                + (np.multiply(np.logical_not(ay), D1n.dot(v * (ww + ww_rand))))
-
-                - kin_vis * (Lap0.matrix * (ww + ww_rand))
-                )
-        CFL = 0.8
-        dt = h * CFL/max(np.abs(np.append(u, v)))
-
-    if loop_count == 0:
-        im = plt.imshow((ww + ww_rand).reshape(gridshape))
-        cbar = plt.colorbar()
-    else:
-
-        norm = colors.Normalize(np.min(ww), np.max(ww))
-
-        im.set_data((ww).reshape(gridshape))
-        im.set_norm(norm)
-    plt.pause(1)
-
-    loop_count += 1
-
-
-# print(timeit.timeit(lambda: splinalg.spsolve(Lap1.matrix, ww),number = 10000))
-
-# plt.imshow((psi_innen+psi_rand).reshape(gridshape))
-# cbar = plt.colorbar()
-#
-# plt.pause(1)
-# plt.imshow(v.reshape(gridshape))
-# cbar.update_normal(plt.gca())
-# plt.pause(1)
-# plt.imshow(u.reshape(gridshape))
-# cbar.update_normal(plt.gca())
-# plt.pause(1)
-# plt.imshow(WW+ww_rand.reshape(gridshape))
-# cbar.update_normal(plt.gca())
-# plt.pause(1)
-# plt.show()
+for ww in rk4(rhs, ww, 1000):
+plt.imshow(rhs(ww,0).reshape(gridshape))
+plt.show()
